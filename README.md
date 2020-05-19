b7df8460509f6049b1f03a218e2d8db765012f85
daybysay <yukita.sekai+github@gmail.com>
2020-05-19 11:25:38 +0900

    initial commit with 'swift package init --type executable'

---
+++ README.md
@@ -0,0 +1,3 @@
+# Study-Swift-CLI
+
+A description of this package.
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import Study_Swift_CLITests
+
+var tests = [XCTestCaseEntry]()
+tests += Study_Swift_CLITests.allTests()
+XCTMain(tests)
